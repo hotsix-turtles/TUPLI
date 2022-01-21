33dee33d93cb611136fc29756eca2a971c3da4b1
kimkisol <kimkisol2@gmail.com>
2022-01-21 11:56:52 +0900

    S06P12A102-275 #comment feature:frontend: vuetify 설치, vuex/components/views 대분류 단위 모듈화

--- front/README.md
+++ front/README.md
@@ -3,5 +3,9 @@
 **[개발 환경]**
 
 npm i axios
+
 vue add router
+
 vue add vuex
+
+vue add vuetify
--- front/src/main.js
+++ front/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
