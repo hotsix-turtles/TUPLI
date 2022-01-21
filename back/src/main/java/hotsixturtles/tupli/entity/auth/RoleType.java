79c10588711a2d4c1201a10714880ba90912d5ae
k-min9 <mingu4969@gmail.com>
2022-01-21 15:32:06 +0900

    S06P12A102-65 #done #comment feature:backend usermanagement repository

---
+++ back/src/main/java/hotsixturtles/tupli/entity/auth/RoleType.java
@@ -0,0 +1,24 @@
+package hotsixturtles.tupli.entity.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum RoleType {
+    USER("ROLE_USER", "일반 사용자 권한"),
+    ADMIN("ROLE_ADMIN", "관리자 권한"),
+    GUEST("GUEST", "게스트 권한");
+
+    private final String code;
+    private final String displayName;
+
+    public static RoleType of(String code) {
+        return Arrays.stream(RoleType.values())
+                .filter(r -> r.getCode().equals(code))
+                .findAny()
+                .orElse(GUEST);
+    }
+}
