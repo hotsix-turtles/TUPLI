cbb54dfcc93fbea958f21b60a64e2ec8e23503ae
김형준 <ninini6630@gmail.com>
2022-01-23 18:18:30 +0900

    S06P12A102-157 #in-progress #comment feature:front:add vuex module for playroom

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -3,6 +3,8 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import playroom from './modules/playroom.js'
+
 export default new Vuex.Store({
   state: {
   },
@@ -11,5 +13,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    playroom: playroom
   }
 })
