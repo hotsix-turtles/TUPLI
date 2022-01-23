2c42392ef8050a5b143737cd4514a7250b03b3c5
김형준 <ninini6630@gmail.com>
2022-01-23 18:21:10 +0900

    S06P12A102-157 #in-progress #comment feature:front:implement and bind some elements (dynamic title, bind vuex states/getters, basic css)

--- front/src/store/modules/playroom.js
+++ front/src/store/modules/playroom.js
@@ -1,9 +1,27 @@
 const playroom = {
     namespaced: true,
-    state: {},
+    state: {
+      roomTitle: '3일만에 다이어트 포기 선언하게 만든 영상들',
+      roomPublic: false,
+      roomAuthorProfilePic: 'https://www.naver.com',
+      roomAuthorName: '춘식이',
+      roomAuthorFollowerCount: 456,
+      roomStartTime: new Date(2022, 1, 23, 18, 30),
+      roomEndTime: new Date(2022, 1, 23, 20, 30),
+      roomContent: '같이 치맥하면서 먹방 보실분들?\r\n같이 치맥하면서 먹방 보시분들?\r\n같이 치맥하면서 먹방 보시분들?\r\n'.replace(/\r?\n/, '\n'),
+      roomTags: ['먹방', '쯔양', '고기먹방', '고기먹방', '고기먹방', '고기먹방', '고기먹방'],
+      roomPlaylists: [ '0005', '0010' ],
+      roomCurrentPlaylist: '0005',
+      roomVideos: [ '0010', '0011', '0013' ],
+      roomCurrentVideo: '0013',
+    },
     mutations: {},
     actions: {},
-    modules: {}
+    getters: {
+      roomPlayTime: ( {roomStartTime, roomEndTime} ) => `${roomStartTime.getHours()}:${roomStartTime.getMinutes()} - ${roomEndTime.getHours()}:${roomEndTime.getMinutes()}`,
+      roomPublicLabel: ( {roomPublic} ) => roomPublic ? '공개' : '비공개',
+      roomReducedContent: ( {roomContent} ) => roomContent.split(/\r?\n/).slice(0, 2).join('\n')
+    }
   };
    
 export default playroom;
--- front/src/views/playroom/PlayroomDetail.vue
+++ front/src/views/playroom/PlayroomDetail.vue
@@ -13,34 +13,31 @@
     </div>
     <div class="playroomInfo">
       <div class="playroomTitleWrapper">
-        <div class="playroomPublicBadge">
-
-        </div>
-        <div class="playroomTitle">
-
-        </div>
+        <p v-bind:class="{ playroomPublicBadge: roomPublic, playroomPrivateBadge: !roomPublic }">
+          {{ roomPublicLabel }}
+        </p>
+        <p class="playroomTitle">
+          {{ roomTitle }}
+        </p>
       </div>
       <div class="playroomAuthorWrapper">
         <div class="authorProfilePic">
-
+          <img v-bind:src="roomAuthorProfilePic" />
         </div>
-        <div class="authorName">
-
-        </div>
+        <span class="authorName">{{ roomAuthorName }}</span>
         <div class="authorFollowerWrapper">
-          <span class="authorFollowerLabel">팔로워</span>
-          <span class="authorFollower"></span>
+          <span class="authorFollower">{{ roomAuthorFollowerCount }}</span>
         </div>
       </div>
       <div class="playroomPlaytimeWrapper">
-        <span class="playtimeLabel">PLAY TIME</span>
-        <span class="playtime"></span>
+        <p class="playtime">{{ roomPlayTime }}</p>
       </div>
       <div class="playroomContentWrapper">
-        <p class="playroomContent"></p>
-        <p class="playroomContentMore">더 보기</p>
+        <p v-if="!showReducedContent && roomReducedContent != roomContent" class="playroomReducedContent" v-on:click="showReducedContent = !showReducedContent">{{ roomReducedContent }}</p>
+        <p v-else class="playroomContent">{{ roomContent }}</p>
       </div>
-      <div class="playroomTagItems">
+      <div class="playroomTagWrapper">
+        <p v-for="roomTag in roomTags" v-bind:key="roomTag" class="playroomTagItem">{{ roomTag }}</p>
       </div>
     </div>
     <div class="playlistWrapper">
@@ -78,23 +75,171 @@
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex';
+
 export default {
   name: 'PlayroomDetail',
-  metaInfo: {
-    title: 'RoomName',
-    titleTemplate: '%s | Tupli',
-    htmlAttrs: {
-      lang: 'ko-KR'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      //{ name: 'description', content: ''},
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-    ]
+  data() {
+    return {
+      showReducedContent: false
+    }
+  },
+  metaInfo () {
+    return { 
+      //title: this.roomTitle,
+      titleTemplate: `${this.roomTitle} | Tupli`,
+      htmlAttrs: {
+        lang: 'ko-KR'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        //{ name: 'description', content: ''},
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
+  computed: {
+    ...mapState('playroom', [
+      'roomTitle',
+      'roomPublic',
+      'roomAuthorProfilePic',
+      'roomAuthorName',
+      'roomAuthorFollowerCount',
+      'roomStartTime',
+      'roomEndTime',
+      'roomContent',
+      'roomTags',
+      'roomPlaylists',
+      'roomCurrentPlaylist',
+      'roomVideos',
+      'roomCurrentVideo'
+    ]),
+    ...mapGetters('playroom', [
+      'roomPlayTime',
+      'roomPublicLabel',
+      'roomReducedContent'
+    ]),
+    roomContentReduced: () => {
+      return this.roomContent == this.roomReducedContent
+    }
   }
 }
 </script>
 
 <style lang="scss">
 @import '~@/assets/scss/common';
+
+.playroomTitleWrapper {
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+}
+
+.playroomPublicBadge {
+  width: 50px;
+  height: 20px;
+  background: rgb(82, 72, 221);
+  text-align: center;
+  color: white;
+  border-radius: 10px;
+  font-size: 12px;
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.playroomPrivateBadge {
+  display: inline-block;
+  width: 50px;
+  height: 20px;
+  background: rgb(163, 160, 209);
+  text-align: center;
+  color: white;
+  border-radius: 10px;
+  font-size: 12px;
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.playroomTitle {
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.playroomAuthorWrapper {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  padding: 5px;
+  line-height: 45px;
+}
+
+.authorProfilePic {
+  width: 40px;
+  height: 40px;
+  border: 1px #bbb solid;
+  border-radius: 100%;
+  margin: 5px;
+}
+
+.authorName {
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: 3px;
+}
+
+.authorFollower:before {
+  content: '팔로워';
+  margin-right: 3px;
+}
+
+.authorFollower {
+  color: #bbb;
+  font-size: 12px;
+}
+
+.playroomPlaytimeWrapper {
+  padding: 5px;
+}
+
+.playtime:before {
+  content: 'PLAY TIME';
+  margin-right: 3px;
+  font-weight: bold;
+}
+
+.playroomContentWrapper {
+  padding: 5px;
+}
+
+.playroomContent {
+  white-space: pre-line;
+}
+
+.playroomReducedContent {
+  white-space: pre-line;
+}
+
+.playroomReducedContent:after {
+  content: '더 보기';
+  margin-left: 10px;
+  color: #bbb;
+  font-size: 14px;
+}
+
+.playroomTagWrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.playroomTagItem {
+  padding: 3px 10px;
+  margin-left: 5px;
+  background: rgb(82, 72, 221);
+  color: white;
+  border-radius: 10px;
+}
 </style>
