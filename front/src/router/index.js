efa757d7a260eced6c78267999bc7e9d318fe018
김형준 <ninini6630@gmail.com>
2022-01-23 15:49:02 +0900

    S06P12A102-157 #in-progress #comment feature:front:add route item to access the playroom detail page

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PlayroomDetail from '../views/playroom/PlayroomDetail.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/playroom',
+    name: 'PlayroomDetail',
+    component: PlayroomDetail
   },
 ]
 
