0d4021d72804172510a14f7c478e66b6ae7abec0
임다훈 <dahontop@gmail.com>
2022-01-27 22:46:24 +0900

    S06P12A102-387 #done #comment feature:backend: SearchService Board Impl

--- back/src/main/java/hotsixturtles/tupli/service/SearchService.java
+++ back/src/main/java/hotsixturtles/tupli/service/SearchService.java
@@ -1,7 +1,10 @@
 package hotsixturtles.tupli.service;
 
+import hotsixturtles.tupli.dto.params.BoardSearchCondition;
 import hotsixturtles.tupli.dto.params.UserSearchCondition;
+import hotsixturtles.tupli.entity.Board;
 import hotsixturtles.tupli.entity.User;
+import hotsixturtles.tupli.repository.BoardRepositoryImpl;
 import hotsixturtles.tupli.repository.UserRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -15,7 +18,12 @@ public class SearchService {
 
     private final UserRepositoryImpl searchRepository;
 
+    private final BoardRepositoryImpl boardRepository;
+
     public List<User> searchUser(UserSearchCondition userSearchCondition, Pageable pageable){
         return searchRepository.searchByPageSimpleUser(userSearchCondition, pageable);
+    }
+    public List<Board> searchBoard(BoardSearchCondition boardSearchCondition, Pageable pageable){
+        return boardRepository.searchByPageSimpleBoard(boardSearchCondition, pageable);
     }
 }
