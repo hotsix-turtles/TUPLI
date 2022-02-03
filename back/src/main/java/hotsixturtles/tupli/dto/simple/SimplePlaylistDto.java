8cb6beb48c2eef42bf0d41466f06c41d95a069d8
강민구 <mingu4969@naver.com>
2022-02-03 15:55:21 +0900

    S06P12A102-139 #done #comment feature:backend: playlist video add DTO change

--- back/src/main/java/hotsixturtles/tupli/dto/simple/SimplePlaylistDto.java
+++ back/src/main/java/hotsixturtles/tupli/dto/simple/SimplePlaylistDto.java
@@ -5,23 +5,29 @@ import hotsixturtles.tupli.entity.meta.PlaylistInfo;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Data
 public class SimplePlaylistDto {
 
+
     private Long id;
-    private String name;
-    private String description;
+    private String title;
+    private String content;
+    private String tags;
+    private Boolean isPublic;
     private String image;
     private OffsetDateTime createdAt;
     private OffsetDateTime updatedAt;
-    private PlaylistInfo playlistInfo;
+    private ConcurrentHashMap<Integer, Integer> playlistInfo;
 
     public SimplePlaylistDto (Playlist playlist) {
         this.id = playlist.getId();
-        this.name = playlist.getName();
-        this.description = playlist.getDescription();
+        this.title = playlist.getTitle();
+        this.content = playlist.getContent();
+        this.tags = playlist.getTags();
+        this.isPublic = playlist.getIsPublic();
         this.image = playlist.getImage();
         this.createdAt = playlist.getCreatedAt();
         this.updatedAt = playlist.getUpdatedAt();
