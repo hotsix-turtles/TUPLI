cfcd94be3ae8db6def0109a36b39450715c36b26
김형준 <ninini6630@gmail.com>
2022-02-04 14:52:04 +0900

    refactor:front:Componentize NavButton

---
+++ front/src/components/common/NavButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn>
+    <span :style="{ color: color }">{{ content }}</span>
+    <v-icon :color="color">
+      {{ icon }}
+    </v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'NavButton',
+  props: {
+    color: { type: String, default: "white"},
+    content: { type: String, default: "메뉴1"},
+    icon: { type: String, default: "mdi-play"}
+  }
+}
+</script>
+
+<style>
+
+</style>
