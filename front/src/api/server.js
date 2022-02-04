159b63535d55ff7767d019c265f911658cb1cee7
김형준 <ninini6630@gmail.com>
2022-02-04 23:59:54 +0900

    feature:frontend:implement oauth by dahoon

---
+++ front/src/api/server.js
@@ -0,0 +1,6 @@
+export default{
+  URL: "http://127.0.0.1:8080", // Spring쪽 URL
+  BACKEND_PORT: 8080,
+  FRONTEND_PORT: 3000
+}
+
---
+++ front/src/views/handler/AuthHandler.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Please wait...
+  </div>
+</template>
+
+<script>
+import { mapActions, mapMutations } from 'vuex'
+import router from '@/router'
+
+export default {
+  name: 'AuthHandler',
+  created() {
+    const token = this.$route.query.token
+    if (token) {
+      this.SET_TOKEN(token)
+      this.getUserInfoToken(token) // 쿼리로 유저 네임이나 정보도 보내줘야되나...
+    }
+    // this.$router.replace('/')  // 홈으로 보내버리겠다. 2
+    // router.push('/')  // 홈으로 보내버리겠다. 1
+    this.$router.push('/home')
+  },
+  methods: {
+    ...mapActions(['getUserInfoToken']),
+    ...mapMutations(['SET_TOKEN'])
+  }
+}
+</script>
+
+<style>
+
+</style>
