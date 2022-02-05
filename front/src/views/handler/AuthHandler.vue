5eb2cda11d889d0e270982a6dccbba9d5b7524f8
최하영 <devhapeng@gmail.com>
2022-02-06 05:09:38 +0900

    #comment oauth by mingu-kang

--- front/src/views/handler/AuthHandler.vue
+++ front/src/views/handler/AuthHandler.vue
@@ -16,7 +16,7 @@ export default {
       this.TOKEN(token)
       this.getUserInfo() // 쿼리로 유저 네임이나 정보도 보내줘야되나...
     }
-    // this.$router.replace('/')  // 홈으로 보내버리겠다.  2
+    // this.$router.replace('/')  // 홈으로 보내버리겠다. 2
     // router.push('/')  // 홈으로 보내버리겠다. 1
     this.$router.push('/home')
   },
