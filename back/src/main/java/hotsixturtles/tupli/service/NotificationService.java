def12d1a9a6804bded5b62b899ea303571800823
강민구 <mingu4969@naver.com>
2022-02-06 03:07:11 +0900

    S06P12A102-184 #done #comment firebase rtdb setting service

--- back/src/main/java/hotsixturtles/tupli/service/NotificationService.java
+++ back/src/main/java/hotsixturtles/tupli/service/NotificationService.java
@@ -7,36 +7,47 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class NotificationService {
 
     /**
-     * 알림 내용 DB에 저장
+     * 팔로우시 알림
      */
-    public void saveNoti() {
-//        LocalDateTime curDateTime = LocalDateTime.now();
-//        String nowDate = curDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+    public void notiFollow() {
+        String alarmType = "follow";
+        LocalDateTime curDateTime = LocalDateTime.now();
+        String nowDate = curDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
 
         // 저장할 데이터
         NotificationDto notificationDto = new NotificationDto();
-        System.out.println(notificationDto.toString());
+        notificationDto.setType(alarmType);
+        notificationDto.setCreatedAt(nowDate);
+        notificationDto.setTo("mingu4969");  // front 비교용
+        notificationDto.setFrom("민구테스터");
+        notificationDto.setFromId(String.valueOf(1L));  // 라우터용 userSeq
+        notificationDto.setImage("사진주소");
+        notificationDto.setIsRead("false");
 
-//        String type = "민구Test2";
-//        notificationDto.setType(type);
-
+        // 알림 개인 확인용
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("noti"); // 최상위 root: noti // ?? 없으면 만들어주나?
-        String toId = "보내는아이디";
+        DatabaseReference ref = database.getReference("tupliTest"); // 최상위 root
+        String toId = "mingu4969"; //보내는 아이디
 
         DatabaseReference notiRef = ref.child(toId); // 알림 받는 사람의 닉네임
-        DatabaseReference nextNotiRef = notiRef.push(); // 다음 키값으로 푸시
-        String postId = nextNotiRef.getKey(); // 현재 알람의1 키값을 가져옴
+        DatabaseReference nextNotiRef = notiRef.push(); // 다음 키 값으로 푸시
+        String postId = nextNotiRef.getKey(); // 현재 알람의1 키값을 가져옴 (덮어쓰기, 중복 방지)
         DatabaseReference saveNoti = notiRef.child(postId); // to의 아이디 값의 child node
-
         saveNoti.setValueAsync(notificationDto);
 
+        // 실시간 알림용
+        final FirebaseDatabase rtdatabase = FirebaseDatabase.getInstance();
+        DatabaseReference rtref = rtdatabase.getReference("tupliTest").child("realtime");
+        rtref.setValueAsync(notificationDto);
     }
 
 
