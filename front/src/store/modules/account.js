68620621e16c8df85f1a3db9c26bb32436872086
최하영 <devhapeng@gmail.com>
2022-02-06 04:52:51 +0900

    #comment oauth by mingu-kang

---
+++ front/src/store/modules/account.js
@@ -0,0 +1,19 @@
+const account = {
+  state: {
+    // 사용자 정보
+    userId: null,
+    email: null,
+    nickname: null,
+    introduction: null,
+    image: null,
+    is_vip: null,
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+}
+export default account
+
