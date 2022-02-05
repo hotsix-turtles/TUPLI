908ccd0131476321ab0d77a0919a24c403587525
임다훈 <dahontop@gmail.com>
2022-02-05 23:35:55 +0900

    S06P12A102-518 #done #comment feature:backend: Add UserBadge Entity

---
+++ back/src/main/java/hotsixturtles/tupli/entity/UserBadge.java
@@ -0,0 +1,38 @@
+package hotsixturtles.tupli.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "user_badge")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserBadge {
+
+    @Id
+    @Column(name = "USER_BADGE_SEQ")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userBadgeSeq;
+
+    private Long userSeq;
+
+    private Long badgeSeq;
+
+    @Column(name = "ACQUIRED_AT")
+    private OffsetDateTime acquired;
+
+    @PrePersist
+    private void beforeSaving() {
+        acquired = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    private void beforeUpdating() {
+        acquired = OffsetDateTime.now();
+    }
+
+
+}
