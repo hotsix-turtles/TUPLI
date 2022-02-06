9d96b90ac0a5ff6a22da2499cdeac80901f94645
임다훈 <dahontop@gmail.com>
2022-02-06 18:08:38 +0900

    S06P12A102-532 #done #comment feature:backend: Add Enabling Spring Scheduler

--- back/src/main/java/hotsixturtles/tupli/TupliApplication.java
+++ back/src/main/java/hotsixturtles/tupli/TupliApplication.java
@@ -4,8 +4,10 @@ import hotsixturtles.tupli.config.auth.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties({
 		AppProperties.class
 })
