7d84c04c56fc161738ce7aa6e0f967f29e0a1389
강민구 <mingu4969@naver.com>
2022-02-06 17:53:52 +0900

    S06P12A102-512

--- front/src/views/accounts/Signup1.vue
+++ front/src/views/accounts/Signup1.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <div class="background-login px-5">
-      <h1>회원가입 페이지</h1>
+      <!-- <h1>회원가입 페이지</h1> -->
       <v-container>
         <router-link
           to="/login"
