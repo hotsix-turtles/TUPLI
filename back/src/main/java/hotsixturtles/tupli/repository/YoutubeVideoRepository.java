c511f06994e730071e15d70b874ecab564b7552e
강민구 <mingu4969@naver.com>
2022-02-07 01:45:06 +0900

    S06P12A102-188 #done #comment feature:backend: connecting playroom repository

--- back/src/main/java/hotsixturtles/tupli/repository/YoutubeVideoRepository.java
+++ back/src/main/java/hotsixturtles/tupli/repository/YoutubeVideoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface YoutubeVideoRepository extends JpaRepository<YoutubeVideo, Long> {
 
     YoutubeVideo findByVideoId(String url);
+
+    YoutubeVideo findOneByVideoId(String url);
 }
