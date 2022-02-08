c511500e0fcf51760df41eab8d668d3a84d05ad7
김형준 <ninini6630@gmail.com>
2022-02-08 15:48:19 +0900

    fix:front:Fix axiosConnector (helped by KYS coach)

--- front/src/utils/axios-connector.js
+++ front/src/utils/axios-connector.js
@@ -1,12 +1,30 @@
 import axios from "axios";
 
 const baseURL = "https://i6a102.p.ssafy.io/api/v1"
-// const baseURL = "http://localhost:8080"
-const token = localStorage.getItem('jwt')
+const axiosConnector = axios.create({
+  baseURL
+})
+
+axiosConnector.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('jwt')
+
+    if (token)
+    {
+      config.headers['Authorization'] = token;
+      // true로 하면 cors
+      // config.withCredentials = false;
+    } else {
+      config.withCredentials = false;
+    }
+
+    return config;
+  },
+  (error) => {
+
+    return Promise.reject(error);
+  }
+);
 
 // axios 객체 생성
-export default axios.create({
-  baseURL,
-  headers: token ? { Authorization: token } : { },
-  xhrFields: token ? { withCredentials: true } : { },
-});
+export default axiosConnector;
