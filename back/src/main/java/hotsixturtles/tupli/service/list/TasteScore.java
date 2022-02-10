7fc66c06fbb3d61649af6d0cecfdd176d2d9b7b0
강민구 <mingu4969@naver.com>
2022-02-10 21:47:39 +0900

    S06P12A102-116 #In-progress #comment feature:backend: user taste analyze list

---
+++ back/src/main/java/hotsixturtles/tupli/service/list/TasteScore.java
@@ -0,0 +1,12 @@
+package hotsixturtles.tupli.service.list;
+
+public class TasteScore {
+
+    public static final Integer SCORE_PLAYLIST_MAKE = 3;
+
+    public static final Integer SCORE_PLAYLIST_LIKE = 3;
+
+
+
+
+}
