70880eae52900ab9cc79a750973456ccf67cc6e8
강민구 <mingu4969@naver.com>
2022-02-11 00:23:17 +0900

    feature:backend: alarm reset all and detail

--- back/src/main/java/hotsixturtles/tupli/api/NotificationApiController.java
+++ back/src/main/java/hotsixturtles/tupli/api/NotificationApiController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +17,26 @@ public class NotificationApiController {
 
     private final NotificationService notificationService;
 
+    @GetMapping("/noti/reset")
+    public void notiReset() {
+        notificationService.realtimeNotiReset();
+    }
+
+    @GetMapping("/noti/read/{id}/id/{notiId}")
+    public void notiRead(@PathVariable("id") String id,
+                        @PathVariable("notiId") String notiId) {
+        notificationService.notiRead(id, notiId);
+    }
+
+    @GetMapping("/noti/readAll/{id}")
+    public void notiReadAll(@PathVariable("id") String id) {
+        notificationService.notiReadAll(id);
+    }
+
+    // Test 생성 코드
     @GetMapping("/saveAlarm")
     public ResponseEntity<?> saveAlarm() {
         notificationService.notiTest();
         return ResponseEntity.status(HttpStatus.OK).body(null);
-    }
-
-    @GetMapping("/notiReset")
-    public void notiReset() {
-        notificationService.realtimeNotiReset();
     }
 }
