152c8ff1fc568a8dc44feca6343804dd2c0f6bb0
임다훈 <dahontop@gmail.com>
2022-02-13 19:00:59 +0900

    S06P12A102-204 #done #comment feature:back: userInfo, Search keyword repo Impl

--- back/src/main/java/hotsixturtles/tupli/repository/UserInfoRepository.java
+++ back/src/main/java/hotsixturtles/tupli/repository/UserInfoRepository.java
@@ -1,7 +1,11 @@
 package hotsixturtles.tupli.repository;
 
 import hotsixturtles.tupli.entity.meta.UserInfo;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     UserInfo findOneByUserSeq(Long userSeq);
