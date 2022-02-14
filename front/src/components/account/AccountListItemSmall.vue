725d58802ac83cc88b76045f7d5219b24e93ebcd
김형준 <ninini6630@gmail.com>
2022-02-14 18:06:36 +0900

    refactor:front:Split AccountListItem component to AccountListItem and AccountItem

--- front/src/components/account/AccountListItemSmall.vue
+++ front/src/components/account/AccountListItemSmall.vue
@@ -1,50 +1,27 @@
 <template>
   <div>
-    <div
+    <account-item-small
       v-for="(account, idx) in accounts"
       :key="idx"
-    >
-      <!-- <div>{{ account.profileImage }}</div> 나중(+프로필로 이동하기 추가) -->
-      <div class="d-flex align-center my-2">
-        <div>
-          <img
-            class="profile"
-            src="https://pbs.twimg.com/media/E4fuVa0WUAAlO1D.jpg"
-          >
-        </div>
-        <div class="ml-3 d-flex-column justify-center">
-          <div class="font-2 semi-bold">
-            {{ account.nickname }}
-          </div>
-          <div class="color-dark-gray">
-            {{ account.email }}
-          </div>
-          <div class="color-dark-gray">
-            팔로워 {{ account.followerCnt }}명
-          </div>
-        </div>
-      </div>
-      <hr class="hr">
-    </div>
-    <!-- {{ accounts }} -->
+      :account="account"
+      :readonly="readonly"
+      :underline="underline"
+    />
   </div>
 </template>
 
 <script>
+import AccountItemSmall from './AccountItemSmall.vue'
 export default {
+  components: { AccountItemSmall },
   name: 'AccountListItemSmall',
   props: {
     accounts: { type: Array, default() { [] } },
+    readonly: { type: Boolean, defaut: true },
+    underline: { type: Boolean, default: false },
   },
 }
 </script>
 
 <style>
-.profile {
-  border-radius: 100%;
-  border: solid 3px;
-  border-color: lightgrey;
-  width: 110px;
-  height: 110px;
-}
 </style>
