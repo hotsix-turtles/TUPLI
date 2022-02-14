b093e9a827f1952c3b6b8a3e2076c716f4c42dbd
임다훈 <dahontop@gmail.com>
2022-02-14 11:19:26 +0900

    hotfix:backend: MainPage Dto Add likes column

--- back/src/main/java/hotsixturtles/tupli/dto/simple/home/SimpleHomeBoardDto.java
+++ back/src/main/java/hotsixturtles/tupli/dto/simple/home/SimpleHomeBoardDto.java
@@ -5,6 +5,9 @@ import hotsixturtles.tupli.dto.PlayroomDto;
 import hotsixturtles.tupli.dto.simple.SimpleBadgeDto;
 import hotsixturtles.tupli.dto.simple.SimpleUserDto;
 import hotsixturtles.tupli.entity.Board;
+import hotsixturtles.tupli.entity.User;
+import hotsixturtles.tupli.entity.likes.BoardLikes;
+import hotsixturtles.tupli.entity.likes.PlaylistLikes;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
@@ -37,6 +40,7 @@ public class SimpleHomeBoardDto {
 
     // 제작 변수
     private Integer likes_count;
+    private String userLikesYN;
 
     public SimpleHomeBoardDto(Board board) {
         this.id = board.getId();
@@ -61,6 +65,42 @@ public class SimpleHomeBoardDto {
         // 변수
         this.likes_count = board.getBoardLikes() == null ? 0 : board.getBoardLikes().size();
 
+    }
+
+    public SimpleHomeBoardDto(Board board, User user) {
+        this.id = board.getId();
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.createdAt = board.getCreated();
+        this.updatedAt = board.getUpdated();
+        this.contentType = board.getType();
+
+        // 유저
+        this.userId = board.getUser().getUserSeq();
+        this.nickName = board.getUser().getNickname();
+        this.userProfileImg = board.getUser().getProfileImage();
+        // 연결
+        if(this.contentType.equals("playroom")){
+            this.contents = new SimpleHomePlayroomDto(board.getPlayroom());
+        }
+        else if(this.contentType.equals("playlist")){
+            this.contents = new SimpleHomePlaylistDto(board.getPlaylist());
+        }
+
+        // 변수
+        this.likes_count = board.getBoardLikes() == null ? 0 : board.getBoardLikes().size();
+
+        if(board.getBoardLikes() == null){
+            this.userLikesYN = "N";
+        }
+        else{
+            for(BoardLikes nowBoardLikes : board.getBoardLikes()){
+                if(nowBoardLikes.getUser().getUserSeq() == user.getUserSeq()){
+                    this.userLikesYN = "Y";
+                    break;
+                }
+            }
+        }
     }
 
 }
