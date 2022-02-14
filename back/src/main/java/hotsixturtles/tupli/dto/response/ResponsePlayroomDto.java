f304b897a26fbf90bc714aa487eb5cabdb6a31f1
임다훈 <dahontop@gmail.com>
2022-02-14 18:09:56 +0900

    fix:backend: Add Playroom isLiked

--- back/src/main/java/hotsixturtles/tupli/dto/response/ResponsePlayroomDto.java
+++ back/src/main/java/hotsixturtles/tupli/dto/response/ResponsePlayroomDto.java
@@ -4,6 +4,8 @@ import hotsixturtles.tupli.dto.PlaylistDto;
 import hotsixturtles.tupli.dto.simple.SimpleUserDto;
 import hotsixturtles.tupli.dto.simple.SimpleYoutubeVideoDto;
 import hotsixturtles.tupli.entity.Playroom;
+import hotsixturtles.tupli.entity.User;
+import hotsixturtles.tupli.entity.likes.PlayroomLikes;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -35,6 +37,8 @@ public class ResponsePlayroomDto {
     private Integer likesCnt;
     private Integer userCountMax;
 
+    private Boolean isLiked;
+
     public ResponsePlayroomDto(Playroom playroom) {
         this.id = playroom.getId();
         this.title = playroom.getTitle();
@@ -53,6 +57,39 @@ public class ResponsePlayroomDto {
         this.user = new SimpleUserDto(playroom.getUser());
         this.videos = playroom.getVideos()
                 .stream().map(x -> new SimpleYoutubeVideoDto(x)).collect(Collectors.toList());
+
+    }
+    public ResponsePlayroomDto(Playroom playroom, User user) {
+        this.id = playroom.getId();
+        this.title = playroom.getTitle();
+        this.content = playroom.getContent();
+        this.isPublic = playroom.getIsPublic();
+        this.tags = playroom.getTags();
+        this.playlists = playroom.getPlaylists();
+        this.inviteIds = inviteIds;
+        this.startTime = playroom.getStartTime();
+        this.endTime = playroom.getEndTime();
+        this.userCount = playroom.getUserCount();
+        this.likesCnt = playroom.getLikesCnt();
+        this.userCountMax = playroom.getUserCountMax();
+
+        // 연결
+        this.user = new SimpleUserDto(playroom.getUser());
+        this.videos = playroom.getVideos()
+                .stream().map(x -> new SimpleYoutubeVideoDto(x)).collect(Collectors.toList());
+
+        if(playroom.getPlayroomLikes() == null){
+            this.isLiked = false;
+        }
+        else{
+            this.isLiked = false;
+            for(PlayroomLikes nowPlayroomLikes : playroom.getPlayroomLikes()){
+                if(nowPlayroomLikes.getUser().getUserSeq() == user.getUserSeq()){
+                    this.isLiked = true;
+                    break;
+                }
+            }
+        }
 
     }
 
