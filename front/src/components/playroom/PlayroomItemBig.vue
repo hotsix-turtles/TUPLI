67108578102ce67343eac6b83d250db06a103ddf
최하영 <devhapeng@gmail.com>
2022-02-18 12:28:35 +0900

    hotfix: 시청자 수 표기 아이콘 padding 조정 duration-big

--- front/src/components/playroom/PlayroomItemBig.vue
+++ front/src/components/playroom/PlayroomItemBig.vue
@@ -20,7 +20,6 @@
           <v-icon
             color="white"
             class="mr-1"
-            style="padding-right: 6px; padding-left: 2px: padding-bottom:4px;"
             small
           >
             mdi-account-multiple
